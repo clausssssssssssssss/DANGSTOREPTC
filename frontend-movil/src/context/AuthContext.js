61112ed10088e1f6cf936a6d1fdfaf99d3fa5a30
Claudia <claudiamariadream@gmail.com>
2025-08-20 10:36:19 -0600

    package json

--- frontend-movil/src/context/AuthContext.js
+++ frontend-movil/src/context/AuthContext.js
@@ -11,8 +11,7 @@ export const AuthProvider = ({ children }) => {
   const [authToken, setAuthToken] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  const API_URL = "http://10.10.4.241:4000/api";
-
+  const API_URL = "http://10.10.0.253:4000/api";
 
   useEffect(() => {
     const loadToken = async () => {
